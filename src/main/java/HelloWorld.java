3805c4b5c3543213a7f4ed9f6f661e3c2fed7713
RickShaa <r.shaffer@gmx.de>
2021-11-13 08:45:15 +0100

    commit test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(8080);
         app.get("/", ctx -> ctx.result("Ich brauch dringend a Glühmopped"));
+        app.get("/test", ctx -> ctx.html("<h1> Servus </h1>"));
     }
 }
