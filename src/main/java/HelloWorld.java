fd42413d458d5f133de868abed4ec54a8cc5c357
Nico-Mayer <nico.m94@icloud.com>
2021-11-13 10:52:31 +0100

    thymeleaf

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,11 +1,31 @@
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class HelloWorld {
+    static String url = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11450649";
+    static String user = "sql11450649";
+    static String password = "b8XJHtWxnq";
+    static Statement stmt = null;
+
     public static void main(String[] args) {
+        // DATENBANK CONNECTION
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, user,password);
+            System.out.println("Connection Succsessful");
+            stmt = conn.createStatement();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        // ROUTES
         Javalin app = Javalin.create(config -> {
             config.addStaticFiles("/public", Location.CLASSPATH);
         }).start(8080);
@@ -20,9 +40,10 @@ public class HelloWorld {
             map.put("mylist",y);
             ctx.render("/template.html", map);
         });
-
         app.post("/changelog", ctx -> {
             ctx.html("Success");
+            stmt.executeUpdate("INSERT INTO CHANGE_LOG VALUES (0, 'TEST', 'TESTCON', 'NM')");
         });
     }
 }
+
