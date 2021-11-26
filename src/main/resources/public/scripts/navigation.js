9f8d35107e7e1c28a0d358e05eef81670bf2ed48
RickShaa <r.shaffer@gmx.de>
2021-11-26 13:26:04 +0100

    added faulty link activation

--- src/main/resources/public/scripts/navigation.js
+++ src/main/resources/public/scripts/navigation.js
@@ -1,18 +1,7 @@
-const url = window.location.href;
-const arr = url.split('/');
-
-console.log(arr[3]);
-if(arr[3].length === 0){
-    const home = document.querySelector("a.general-nav-links[href='/']");
-    home.classList.add("active");
-}else{
-    let projectLinks = document.querySelectorAll("a[data-link='navlink']");
-    projectLinks.forEach(function(e){
-        if(e.getAttribute("href") === "/project" + arr[3].split("t")[1]){
-            e.classList.add("active");
-        }
-    })
-
-}
-
-
+const url = location.href;
+let projectLinks = document.querySelectorAll("[data-link='navlink']");
+projectLinks.forEach(function(e){
+    if(e.href === url){
+        e.classList.add("active");
+    }
+})
