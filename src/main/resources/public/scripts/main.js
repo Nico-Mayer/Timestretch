f546091b3a58511ab12b5fe35b0a68834156237c
RickShaa <r.shaffer@gmx.de>
2021-11-26 11:41:47 +0100

    added settings modal

--- src/main/resources/public/scripts/main.js
+++ src/main/resources/public/scripts/main.js
@@ -4,11 +4,12 @@ const backdrop = document.querySelector("#modal-backdrop");
 const modalP = document.querySelector("#modal-container-project");
 const modalT = document.querySelector("#modal-container-task");
 const modalC = document.querySelector("#modal-container-confirm");
-
+const modalS = document.querySelector("#modal-container-project-settings");
 const addProjectBtn = document.getElementById("add-project-btn");
 const addTaskBtn = document.getElementById("add-task-btn");
-const deleteBtnArray = document.querySelectorAll(".project-delete-btn");
 
+const deleteBtnArray = document.querySelectorAll(".project-delete-btn");
+const settingsBtnArray = document.querySelectorAll(".project-settings-btn");
 const projectModal = new Modal(
     "project",
     backdrop,
@@ -21,8 +22,16 @@ const projectModal = new Modal(
 if (deleteBtnArray !== null){
     deleteBtnArray.forEach(function(e){
         e.addEventListener("click", function(){
-            let deleteModal = new Modal("", backdrop, modalC, e, "/deleteProject" + e.id)
+            let deleteModal = new Modal("", backdrop, modalC, e, e.id)
             deleteModal.set();
+        })
+    })
+}
+if (settingsBtnArray !== null){
+    settingsBtnArray.forEach(function(e){
+        e.addEventListener("click", function(){
+            let settingsModal = new Modal("settings", backdrop, modalS, e, e.id)
+            settingsModal.set();
         })
     })
 }
--- src/main/resources/public/scripts/modal.js
+++ src/main/resources/public/scripts/modal.js
@@ -57,6 +57,37 @@ export default function Modal(type, backdrop, modal, showTrigger, url) {
                     .getElementById("task-btn-cancel")
                     .addEventListener("click", this.hideBtn.bind(null,this.backdrop,this.modal));
                 break;
+
+            case "settings":
+                this.modal.innerHTML = ` 
+        <div class='form-container'>
+        <h2>Projekt bearbeiten</h2>
+        <form method='post' action=${this.url}>
+  <div class="mb-3">
+    <label for="projektName" class="form-label">Neuer Projekt Name</label>
+    <input type="text" class="form-control" id="projekt-name" name='projektName' required="required">
+    <div id="proekt-name-help" class="form-text">Geben Sie Ihrem Projekt einen Eindeutigen Namen</div>
+  </div>
+  <div class="mb-3 form-check">
+                    <label for="color">Farbe</label>
+                    <select name="color" id="color" style="width: 50px">
+                        <option value="#264653" style="background-color: #264653"></option>
+                        <option value="#2a9d8f" style="background-color: #2a9d8f"></option>
+                        <option value="#e9c46a" style="background-color: #e9c46a"></option>
+                        <option value="#f4a261" style="background-color: #f4a261"></option>
+                        <option value="#e76f51" style="background-color: #e76f51"></option>
+                    </select>
+                </div>
+  <button type="submit" class="btn btn-danger">Projekt ändern</button>
+  <button type="button" class="btn btn-outline-secondary" id='project-settings-btn-cancel'> Abbrechen </button>
+</form>
+</div>`;
+                document
+                    .getElementById("project-settings-btn-cancel")
+                    .addEventListener("click", this.hideBtn.bind(null,this.backdrop,this.modal));
+
+                break;
+
             default:
                 this.modal.innerHTML = ` 
         <div class='form-container'>
