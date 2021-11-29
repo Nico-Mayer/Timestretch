3acb9efecfa2e13631618a6ee02668c6fe40a974
Nico-Mayer <nico.m94@icloud.com>
2021-11-29 18:15:52 +0100

    Task funktion wieder eingebaut

--- src/main/resources/public/scripts/main.js
+++ src/main/resources/public/scripts/main.js
@@ -6,10 +6,23 @@ const modalT = document.querySelector("#modal-container-task");
 const modalC = document.querySelector("#modal-container-confirm");
 const modalS = document.querySelector("#modal-container-project-settings");
 
-const addTaskBtn = document.getElementById("add-task-btn");
+// Content Switcher for Single Page use
+const path = window.location.pathname;
+let homeContentView = document.getElementById("home-content-view");
+let projectContetView = document.getElementById("project-content-view")
+
+if(path === "/"){
+    projectContetView.style.display = "none";
+    document.getElementById("addBtn").classList.add("addProjectBtn")
+}else{
+    homeContentView.style.display = "none";
+    document.getElementById("addBtn").classList.add("add-task-btn")
+}
+
+const addTaskBtn = document.querySelector(".add-task-btn");
+console.log(addTaskBtn);
 const deleteBtnArray = document.querySelectorAll(".project-delete-btn");
 const settingsBtnArray = document.querySelectorAll(".project-settings-btn");
-
 const addProjectBtnArray = document.querySelectorAll(".addProjectBtn");
 
 
@@ -50,19 +63,12 @@ if (settingsBtnArray !== null){
 if(addTaskBtn !== null){
     const projectID = window.location.href.substr(29); // Verbesserung suchen
     //console.log(projectID);
-    const taskModal = new Modal("task", backdrop,modalT,addTaskBtn, "/addNewTask"+projectID);
+    const taskModal = new Modal("task", backdrop,modalT,addTaskBtn, "/addNewTask" + projectID);
     taskModal.set();
 }
 
-// Content Switcher for Single Page use
-const path = window.location.pathname;
-let homeContentView = document.getElementById("home-content-view");
-let projectContetView = document.getElementById("project-content-view")
-if(path == "/"){
-    projectContetView.style.display = "none";
-}else{
-    homeContentView.style.display = "none";
-}
+
+
 
 
 const collapseBtn = document.getElementById("done-container-collapse-btn");
